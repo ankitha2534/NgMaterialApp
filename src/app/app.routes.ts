9723cd905a929ba128e8602b1d8911b108250798
Ankitha Reddy Kunam <kunamankithareddy@gmail.com>
2024-08-22 22:42:26 +0530

    added side bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './modules/auth/login/login.component';
+import { SideBarComponent } from './shared/header/side-bar/side-bar.component';
 
 export const routes: Routes = [
-    {path:'',component:LoginComponent},
+    {path:'',component:LoginComponent, pathMatch: 'full' },
     {path:'Login',component:LoginComponent},
     {path:'SignUp',component:LoginComponent},
+    {path:'Dashboard',component:SideBarComponent}
 ];

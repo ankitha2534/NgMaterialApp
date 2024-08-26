458c56bd01d594606dd52540e7f4ea43e07148ad
Ankitha Reddy Kunam <kunamankithareddy@gmail.com>
2024-08-26 23:18:03 +0530

    added cards in dashboard1

--- src/app/modules/components/dashboard1/dashboard1.component.ts
+++ src/app/modules/components/dashboard1/dashboard1.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-dashboard1',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,MatCardModule,MatProgressBarModule],
   templateUrl: './dashboard1.component.html',
   styleUrl: './dashboard1.component.css'
 })
 export class Dashboard1Component {
-
+  financialMetrics : any[] =[
+    {type:'Investment',period:'Monthly',ammount:'$47000',revenueTitle:'Total Revenue',revenuePercent:'55',percent:'%'},
+    {type:'Sales',period:'Annual',ammount:'$22784',revenueTitle:'Total Revenue',revenuePercent:'42',percent:'%'},
+    {type:'Cost',period:'Today',ammount:'$1587',revenueTitle:'Total Revenue',revenuePercent:'75',percent:'%'},
+    {type:'Profit',period:'Weekly',ammount:'$1400',revenueTitle:'Total Revenue',revenuePercent:'48',percent:'%'}
+  ]
 }
